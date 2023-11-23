b2deb5f6a18e9988f9d360fcc32808b5e8c8aca7
Bonk <bonk@h295.aau1x.klient.sydhavn2.site.aau.dk>
2023-11-23 10:07:22 +0100

    Musik Script
    Musik stopper naar man naar scene 4 og 5. Skal stadig have MainMusic til at starte igen hvis man restarter

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,12 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
-{ 
-    private void Awake()
+{
+    public AudioSource myAudioSource;
+    
+    // Method to keep music active through scenes
+    void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
+        DontDestroyOnLoad(myAudioSource);
     }
 
+    // Turns music off if in scene index 4 or 5
+    void StopMusic()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 4)
+        {
+            myAudioSource.Stop();
+        }
+        if (SceneManager.GetActiveScene().buildIndex == 5)
+        {
+            myAudioSource.Stop();
+        }
+    }
+    
+    private void Update()
+    {
+        StopMusic();
+    }
 }
