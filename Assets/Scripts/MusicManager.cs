ead2b503813ac6d38ed0376eabb5c2b38eefda00
Bonk <bonk@h295.aau1x.klient.sydhavn2.site.aau.dk>
2023-11-23 12:01:51 +0100

    Leget med MusicManager

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,23 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
     public AudioSource myAudioSource;
-    
+
     // Method to keep music active through scenes
+    
     void Awake()
     {
         DontDestroyOnLoad(myAudioSource);
     }
+    
 
     // Turns music off if in scene index 4 or 5
+    
+    
     void StopMusic()
     {
-        if (SceneManager.GetActiveScene().buildIndex == 4)
+        if (SceneManager.GetActiveScene().buildIndex == 2)
         {
             myAudioSource.Stop();
         }
@@ -26,9 +31,23 @@ public class MusicManager : MonoBehaviour
             myAudioSource.Stop();
         }
     }
+
+    
+    /*
+    void RestartMainMusic()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 3 && myAudioSource != myAudioSource.isPlaying);
+        {
+          myAudioSource.Play();
+        }
+        
+    }
+    */
     
     private void Update()
     {
         StopMusic();
+        //RestartMainMusic();
     }
+    
 }
